2c53b3d31e113fccfd43d6a85817882a442361d0
Xiaolin Zhang <leoncamel@gmail.com>
2019-09-18 21:09:46 +0800

    Switch to new cli parser

--- main.go
+++ main.go
@@ -1,70 +1,50 @@
 package main
 
 import (
-	"flag"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net"
 	"os"
-	"strconv"
 	"strings"
 	"time"
 
+	"github.com/urfave/cli"
 	"gortc.io/stun"
 )
 
-func main() {
-	var err error
+func runStunClient(c *cli.Context) {
+	addr := c.String("server")
+	sleepDuration := c.Int("sleep")
+	rto := c.Int("rto")
+	totalCount := c.Int("count")
 
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
-		fmt.Fprintln(os.Stderr, os.Args[0], "stun1.l.google.com:19302")
-	}
-
-	flag.Parse()
-	addr := flag.Arg(0)
-	if addr == "" {
-		addr = "stun1.l.google.com:19302"
-	}
-
-	var sleepStr string = flag.Arg(1)
-	if sleepStr == "" {
-		sleepStr = "0"
-	}
-	var sleepDuration int
-	sleepDuration, err = strconv.Atoi(sleepStr)
-	if err != nil {
-		fmt.Println("Invalid sleep duration:", sleepStr)
-		panic(err)
-	}
-
-	var rtoStr string = flag.Arg(2)
-	if rtoStr == "" {
-		rtoStr = "300"
-	}
-	var rto int
-	rto, err = strconv.Atoi(rtoStr)
 	// ====================
-	fmt.Println("target stun server                  : ", addr)
-	fmt.Println("sleep interval for each iteration is: ", sleepStr)
 	showNetInterfaces(false)
+	fmt.Println("target stun server                  : ", addr)
+	fmt.Println("sleep interval for each iteration is: ", sleepDuration)
 
 	// Creating a "connection" to STUN server.
-	c, err := stun.Dial("udp", addr)
+	client, err := stun.Dial("udp", addr)
 	if err != nil {
 		panic(err)
 	}
 
-	c.SetRTO(time.Duration(rto) * time.Millisecond)
+	client.SetRTO(time.Duration(rto) * time.Millisecond)
 
-	for index := 0; index < 10; index++ {
+	for index := 0; index < totalCount; index++ {
 		// Building binding request with random transaction id.
 		message := stun.MustBuild(stun.TransactionID, stun.BindingRequest)
 
 		// Sending request to STUN server, waiting for response message.
-		if err := c.Do(message, func(res stun.Event) {
+		if err := client.Do(message, func(res stun.Event) {
 			if res.Error != nil {
 				panic(res.Error)
 			}
+			if c.Bool("verbose") {
+				fmt.Println(serilizeToString(res))
+			}
+
 			// Decoding XOR-MAPPED-ADDRESS attribute from message.
 			var xorAddr stun.XORMappedAddress
 			if err := xorAddr.GetFrom(res.Message); err != nil {
@@ -114,4 +94,57 @@ func showNetInterfaces(verbose bool) {
 		}
 	}
 	fmt.Println("==================================================")
+}
+
+func serilizeToString(e stun.Event) string {
+	out, err := json.Marshal(e)
+	if err != nil {
+		return ""
+	}
+
+	return string(out)
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Version = "0.0.1"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "server, s",
+			Value: "stun1.l.google.com:19302",
+			Usage: "STUN server address in format like IP:UDP_Port",
+		},
+		cli.IntFlag{
+			Name:  "sleep, i",
+			Value: 0,
+			Usage: "Sleep interval for each iteration, in ms",
+		},
+		cli.IntFlag{
+			Name:  "rto, r",
+			Value: 300,
+			Usage: "RTO timeout for each STUN Message, in ms",
+		},
+		cli.IntFlag{
+			Name:  "count, c",
+			Value: 10,
+			Usage: "Loop count",
+		},
+		cli.BoolFlag{
+			Name:  "verbose, V",
+			Usage: "Verbose output",
+		},
+	}
+
+	// sort.Sort(cli.FlagsByName(app.Flags))
+
+	app.Action = func(c *cli.Context) error {
+		runStunClient(c)
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
